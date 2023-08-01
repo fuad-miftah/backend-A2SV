30c90dc16e7d9c56bdbe6410dd48c8db30b44b8d
Fuad Miftah <fuadmiftah9@gmail.com>
2023-08-01 19:46:34 +0300

    word-frequency

---
+++ Day1/word-frequency/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+Dictionary<string,int> frequency(string s){
+    Dictionary<string,int> dic = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+    string[] words = s.Split(' ');
+    foreach(string word in words){
+        if(dic.ContainsKey(word)){
+            dic[word] += 1;
+        }
+        else{
+            dic.Add(word, 1);
+        }
+    }
+    return dic;
+}
+
+string s = "The the two Two";
+var res = frequency(s);
+foreach(var pair in res){
+    Console.WriteLine(pair);
+}
