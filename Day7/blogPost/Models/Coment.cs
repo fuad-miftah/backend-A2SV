17641ef71fd0090ebbc5ff8e5161c3c340e7dded
Fuad Miftah <fuadmiftah9@gmail.com>
2023-08-14 09:51:28 +0300

    blog-post-fixed

--- Day7/blogPost/Models/Coment.cs
+++ Day7/blogPost/Models/Coment.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+﻿using System.Text.Json.Serialization;
 
 
 
@@ -12,5 +13,6 @@ public class Comment : BaseEntity
 {
     public string Text { get; set;} = "";
     public int PostId { get; set;}
-    public virtual Post Post { get; set;}
+    [JsonIgnore]
+    public virtual Post? Post { get; set;}
 }
--- Day7/blogPost/Program.cs
+++ Day7/blogPost/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using blogpost.Data;
+using System.Text.Json.Serialization;
 
 
 
@@ -11,7 +12,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+//builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(x =>
+    x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
