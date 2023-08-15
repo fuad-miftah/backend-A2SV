8148290ce486a264fb97556b689e62734071683c
Fuad Miftah <fuadmiftah9@gmail.com>
2023-08-15 16:40:35 +0300

    blogapi-domain&application-layer

---
+++ Day10/BlogApi/BlogApi.Domain/Comment.cs
@@ -0,0 +1,18 @@
+﻿using BlogApi.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace BlogApi.Domain
+{
+    public class Comment: BaseDomainEntity
+    {
+        public string Text { get; set; } = "";
+        public int PostId { get; set; }
+        [JsonIgnore]
+        public virtual Post? Post { get; set; }
+    }
+}
---
+++ Day10/BlogApi/BlogApi.Domain/Post.cs
@@ -0,0 +1,22 @@
+﻿using BlogApi.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApi.Domain
+{
+    public class Post: BaseDomainEntity
+    {
+        public Post()
+        {
+            Comments = new HashSet<Comment>();
+        }
+
+        public string Title { get; set; } = "";
+        public string Content { get; set; } = "";
+
+        public virtual ICollection<Comment> Comments { get; set; }
+    }
+}
